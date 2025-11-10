88c3e5994cc2e8df5ba4facddab5215e09cf5a99
Rabeet Ahmer <rabeetahmer9749@gmail.com>
2025-11-10 20:38:14 +0500

    Initial commit

---
+++ components/EventCard.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+    slug: string;
+    title: string;
+    image: string;
+    location: string;
+    date: string;
+    time: string
+}
+
+const EventCard = ({title, image, slug, location, date, time}: Props) => {
+  return (
+    <Link href={`/events/${slug}`} id="event-card">
+        <Image src={image} alt= {title} width={410} height={300} className="poster"/>
+
+        <div className="flex gap-2">
+          <Image src={"/icons/pin.svg"} alt="location" width={14} height={14}/>
+          <p>{location}</p>
+        </div>
+    
+        <p className="title">{title}</p>
+    </Link>
+  )
+}
+
+export default EventCard
---
+++ lib/constants.ts
@@ -0,0 +1,51 @@
+export const events = [
+  {
+    slug: "react-conf-2024",
+    title: "React Conf 2024",
+    image: "/images/event1.png",
+    location: "Las Vegas, NV",
+    date: "October 15-16, 2024",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    slug: "google-io-2024",
+    title: "Google I/O 2024",
+    image: "/images/event2.png",
+    location: "Mountain View, CA",
+    date: "May 14-16, 2024",
+    time: "10:00 AM - 5:00 PM"
+  },
+  {
+    slug: "aws-reinvent-2024",
+    title: "AWS re:Invent 2024",
+    image: "/images/event3.png",
+    location: "Las Vegas, NV",
+    date: "December 2-6, 2024",
+    time: "8:00 AM - 7:00 PM"
+  },
+  {
+    slug: "microsoft-build-2024",
+    title: "Microsoft Build 2024",
+    image: "/images/event4.png",
+    location: "Seattle, WA",
+    date: "May 21-23, 2024",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    slug: "pycon-2024",
+    title: "PyCon US 2024",
+    image: "/images/event5.png",
+    location: "Pittsburgh, PA",
+    date: "May 15-23, 2024",
+    time: "8:30 AM - 6:00 PM"
+  },
+  {
+    slug: "jsconf-eu-2024",
+    title: "JSConf EU 2024",
+    image: "/images/event6.png",
+    location: "Berlin, Germany",
+    date: "June 10-11, 2024",
+    time: "9:00 AM - 6:00 PM"
+  }
+];
+
